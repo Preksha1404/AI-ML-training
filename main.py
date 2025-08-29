834a627892ec6a41a360a664d0108c39d3d7edb2
Preksha <ranapreksha8@gmail.com>
2025-08-29 10:17:48 +0530

    Add Employee class and logging functionality

---
+++ main.py
@@ -0,0 +1,28 @@
+# logging levels
+# DEBUG, INFO, WARNING, ERROR, CRITICAL
+# By default, the logging module logs the messages with a logging level of WARNING or above.
+
+import logging
+
+logging.basicConfig(filename='employee.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+class Employee:
+    """A sample Employee class"""
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+        logging.info('Created Employee: {} - {}'.format(self.fullname, self.email))
+
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('John', 'Smith')
+emp_2 = Employee('Corey', 'Schafer')
+emp_3 = Employee('Jane', 'Doe')
